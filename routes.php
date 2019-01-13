12a0c2fc34bf437d54e0659b4def6c18cca81026
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2019-01-13 17:59:05 +0000

    Removed unsed routes

--- routes.php
+++ routes.php
@@ -1,15 +1,8 @@
 <?php
 // Render Twig template within container
 $app->get('/', 'Controllers\\UserController:index');
-$app->get('/property', 'Controllers\\PropertyController:allProperties');
-$app->get('/property/{id}', 'Controllers\\PropertyController:singleProperty');
-$app->post('/editpost/{id}', 'Controllers\\PropertyController:editpost');
-$app->post('/deletepost/{id}', 'Controllers\\PropertyController:deletepost');
-$app->post('/search.{type}', 'Controllers\\PropertyController:search');
 $app->get('/admin', 'Controllers\\UserController:admin');
 $app->get('/admin/{id}', 'Controllers\\UserController:adminById');
 $app->get('/login', 'Controllers\\UserController:login');
 $app->post('/login', 'Controllers\\UserController:loginpost');
 $app->get('/logout', 'Controllers\\UserController:logout');
-$app->get('/edit/{id}', 'Controllers\\PropertyController:edit');
-$app->get('/delete/{id}', 'Controllers\\PropertyController:delete');
