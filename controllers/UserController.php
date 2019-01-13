c4efb6a02b82debcfae7743fea7b9ae7773ff5e1
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2019-01-13 17:57:52 +0000

    formatting and

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,35 +2,39 @@
 
 namespace Controllers;
 
-use \Models\UserModel;
 use \Models\PropertyModel;
+use \Models\UserModel;
 
-class UserController 
+class UserController
 {
     protected $container;
 
-    public function __construct(\Slim\Container $container) {
+    public function __construct(\Slim\Container $container)
+    {
         $this->container = $container;
     }
 
-    public function login($request, $response, $args) {
+    public function login($request, $response, $args)
+    {
         return $this->container->get('view')->render($response, 'login.twig', []);
     }
 
-    public function loginpost($request, $response, $args) {
+    public function loginpost($request, $response, $args)
+    {
         $params = $request->getParsedBody();
         if ($this->validateLogin($params['username'], $params['password'])) {
             $this->container->get('session')->set('username', $params['username']);
-            $this->container->get('session')->set('is_admin',UserModel::where('username', $params['username'])->first()->is_admin);
+            $this->container->get('session')->set('is_admin', UserModel::where('username', $params['username'])->first()->is_admin);
             return $response->withStatus(200)->withRedirect('/admin');
         } else {
             return $response->withRedirect('/login')->withoutHeader('WWW-Authenticate');
         }
     }
 
-    public function validateLogin($username, $password) {
+    public function validateLogin($username, $password)
+    {
         $user = UserModel::where('username', $username)->first();
-        if ($user != NULL) {
+        if ($user != null) {
             if (password_verify($password, $user->password)) {
                 return true;
             } else {
@@ -41,20 +45,27 @@ class UserController
         }
     }
 
-    public function admin($request, $response, $args) {
+    public function admin($request, $response, $args)
+    {
         if ($this->container->get('session')->exists('username')) {
             return $this->container->get('view')->render($response, 'admin.twig', [
                 'admin' => true,
                 'properties' => PropertyModel::all(),
-                ]);
+            ]);
         } else {
             return $response->withRedirect('/login')->withoutHeader('WWW-Authenticate');
         }
     }
 
-    public function logout($request, $response, $args){
+    public function logout($request, $response, $args)
+    {
         $this->container->get('session')->delete('username');
         $this->container->get('session')->delete('is_admin');
         return $response->withStatus(200)->withRedirect('login');
+    }
+
+    public function index($request, $response, $args)
+    {
+        return $response->withStatus(200)->getBody()->write("Hello, world!");
     }
 }
