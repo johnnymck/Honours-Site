0865efed5a92b6b4f16f7533ad5aad67f7664174
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2019-02-07 21:46:31 +0000

    added application route for user approval

--- routes.php
+++ routes.php
@@ -2,6 +2,7 @@
 // Render Twig template within container
 $app->get('/', 'Controllers\\UserController:index');
 $app->get('/admin', 'Controllers\\UserController:admin');
+$app->get('/admin/pending-users', 'Controllers\\UserController:pendingUsers');
 $app->get('/admin/{id}', 'Controllers\\UserController:adminById');
 $app->get('/login', 'Controllers\\UserController:login');
 $app->post('/login', 'Controllers\\UserController:loginpost');
